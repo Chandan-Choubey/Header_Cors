37d8592519387941ad01912e367fd6f61a47305f
Chandan-Choubey <choubeychandan2463@gmail.com>
2025-01-17 20:39:14 +0530

    Add Cors in Header

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/", async (res, req) => {
+  res.send("Hello, world!");
+});
+
+app.get("/proxy-image", async (req, res) => {
+  console.log(req.body);
+  const imageUrl = req.query.url;
+
+  if (!imageUrl) {
+    return res.status(400).json({ error: "Image URL is required" });
+  }
+
+  try {
+    const response = await axios.get(imageUrl, {
+      responseType: "arraybuffer",
+    });
+
+    res.set("Content-Type", response.headers["content-type"]);
+    res.set("Access-Control-Allow-Origin", "*");
+    res.send(response.data);
+  } catch (error) {
+    console.error("Error fetching image:", error.message);
+    res.status(500).json({ error: "Failed to fetch image" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
